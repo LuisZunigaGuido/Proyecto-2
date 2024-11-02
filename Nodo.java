dff2e3acbe7f903dc04f2cbbb39b2fea57890612
LuisZuñigaGuido <luis.guido1507@gmail.com>
2024-11-02 14:47:02 -0600

    en este primer commit se crea las clases que se consideran necesarias para el proyecto 2

---
+++ Nodo.java
@@ -0,0 +1,8 @@
+public class Nodo {
+    private Dato dato1;
+    private Nodo nodoSiguiente;
+    public Nodo (Dato dato1) {
+        this.dato1 = dato1;
+        this.nodoSiguiente = null;
+    }
+}
