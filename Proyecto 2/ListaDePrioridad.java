7503e0ac4ffd15fde21cce89ea162bc318095558
LuisZuñigaGuido <luis.guido1507@gmail.com>
2024-11-11 17:04:05 -0600

    mas avances del metodo y adicion de la clase main pruebas ideada para probar los metodos

--- Proyecto 2/ListaDePrioridad.java
+++ Proyecto 2/ListaDePrioridad.java
@@ -13,18 +13,35 @@ public class ListaDePrioridad {
     }
     public void insertarDatos (int DatoIn, int prioridadIn) {
        Nodo nuevoNodo = new Nodo(DatoIn,prioridadIn); // creamos un nuevo nodo con el dato y la prioridad deseada
-       // si el nodo es nulo entonces el primer nodo insertado es e nodo raiz
+       // si el nodo raiz es nulo entonces el primer nodo insertado es el nodo raiz
        if(this.nodoRaiz == null) {
            nodoRaiz = nuevoNodo;
-       } else {
-           //si no es nullo entonces queremos crear el puntero nodo actual en el nodo raiz y el nodo siguiente sera el ingresado
+       } else{
+           //la prioridad del nodo ingresado es mayor a la del nodo raíz
+           if(prioridadIn>nodoRaiz.prioridad){
+           nuevoNodo.nodoSiguiente = nodoRaiz;
+           nodoRaiz = nuevoNodo;
+           }
+           //si no es nulo entonces queremos cubrir los casos donde el nuevo nodo va al final o el nuevo nodo va en el medio de dos nodos
+           //cuando va al medio de eso se encarga el while y cuando no, se sale del while y entran las ultimas lineas.
            Nodo nodoActual = nodoRaiz;
-           while(nodoActual.nodoSiguiente!=null && nodoActual.nodoSiguiente.prioridad<=prioridadIn){
+           while(nodoActual.nodoSiguiente!=null && nodoActual.nodoSiguiente.prioridad>=prioridadIn){
                nodoActual = nodoActual.nodoSiguiente;
            }
-           nuevoNodo = nodoActual;
            nuevoNodo.nodoSiguiente = nodoActual.nodoSiguiente;
+           nodoActual.nodoSiguiente = nuevoNodo;
        }
+    }
+    //metodo para imprimir lista
+    public void imprimirLista(){
+        Nodo nodoActual = nodoRaiz;
+        if(nodoActual==null){
+            System.out.println("lista vacía");
+        }
+        while(nodoActual!=null){
+            System.out.println("el Dato es "+nodoActual.dato+ "y la prioridad es "+nodoActual.prioridad);
+            nodoActual = nodoActual.nodoSiguiente;
+        }
     }
 }
 //casos si tengo nodo raiz y solo hay raiz, ahora el segundo caso es cuando lo tengo en el medio
