e4baa6c66f257494d1a407f2318efd3afee27a25
LuisZuñigaGuido <luis.guido1507@gmail.com>
2024-11-11 13:54:30 -0600

    adjunto los avances de las clases nodo y el metodo de ingresar datos

--- Proyecto 2/ListaDePrioridad.java
+++ Proyecto 2/ListaDePrioridad.java
@@ -1,3 +1,30 @@
 public class ListaDePrioridad {
-    
+    Nodo nodoRaiz;
+    public ListaDePrioridad(){
+        this.nodoRaiz = null;
+    }
+    //setters
+    public void setNodoRaiz(Nodo nodoRaiz) {
+        this.nodoRaiz = nodoRaiz;
+    }
+    //getters
+    public Nodo getNodoRaiz(){
+        return nodoRaiz;
+    }
+    public void insertarDatos (int DatoIn, int prioridadIn) {
+       Nodo nuevoNodo = new Nodo(DatoIn,prioridadIn); // creamos un nuevo nodo con el dato y la prioridad deseada
+       // si el nodo es nulo entonces el primer nodo insertado es e nodo raiz
+       if(this.nodoRaiz == null) {
+           nodoRaiz = nuevoNodo;
+       } else {
+           //si no es nullo entonces queremos crear el puntero nodo actual en el nodo raiz y el nodo siguiente sera el ingresado
+           Nodo nodoActual = nodoRaiz;
+           while(nodoActual.nodoSiguiente!=null && nodoActual.nodoSiguiente.prioridad<=prioridadIn){
+               nodoActual = nodoActual.nodoSiguiente;
+           }
+           nuevoNodo = nodoActual;
+           nuevoNodo.nodoSiguiente = nodoActual.nodoSiguiente;
+       }
+    }
 }
+//casos si tengo nodo raiz y solo hay raiz, ahora el segundo caso es cuando lo tengo en el medio
--- Proyecto 2/Nodo.java
+++ Proyecto 2/Nodo.java
@@ -1,8 +1,30 @@
 public class Nodo {
-    private Dato dato1;
-    private Nodo nodoSiguiente;
-    public Nodo (Dato dato1) {
-        this.dato1 = dato1;
+    static int dato;
+    static int prioridad;
+    Nodo nodoSiguiente;
+    public Nodo(int dato, int prioridad){
+        this.dato = dato;
+        this.prioridad = prioridad;
         this.nodoSiguiente = null;
+    }
+    //seters
+    public void setDato(int dato){
+        this.dato = dato;
+    }
+    public void setPrioridad(int prioridad){
+        this.prioridad = prioridad;
+    }
+    public void setNodoSiguiente(Nodo nodoSiguiente){
+        this.nodoSiguiente = nodoSiguiente;
+    }
+    //geters
+    public int getDato(){
+        return this.dato;
+    }
+    public int getPrioridad(){
+        return this.prioridad;
+    }
+    public Nodo getNodoSiguiente(){
+        return this.nodoSiguiente;
     }
 }
